c8a87d403b533144941824e7298cb913efcb9ea4
deBroglieWave <zhihangyi0224@gmail.com>
2025-10-30 11:42:07 +0800

    added mazeProblem function

--- Code/homework2.cpp
+++ Code/homework2.cpp
@@ -6,10 +6,14 @@
 #include "LinkedQueue.h"
 #include <string>
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+
 
 void testStack();
 bool bracketMatch(std::string s);
 void testQueue();
+void mazeProblem();
 
 int main(void) {
     testStack();
@@ -20,6 +24,8 @@ int main(void) {
     std::cout << b2 << ": " << bracketMatch(b2) << std::endl;
 
     testQueue();
+
+
 
     return 0;
 }
@@ -106,4 +112,67 @@ void testQueue() {
     q->showAll();
 
     delete q;
+}
+
+void mazeProblem() {
+    int m, n;
+    std::cout << "Enter the size of the maze (m, n)" << std::endl;
+    std::cout << "m: ";
+    std::cin >> m;
+    std::cout << "n: ";
+    std::cin >> n;
+
+    int maze[m][n];
+    bool randomGenerated;
+
+    std::cout << "Do you want to generate the maze randomly, (1 for yes, 0 for no): ";
+    std::cin >> randomGenerated;
+
+    if (randomGenerated) {
+        srand(time(0));
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++)
+                maze[i][j] = rand() % 2;
+        }
+
+    }
+    else {
+        std::cout << "Enter the maze row by row, using 0 for open path and 1 for wall: " << std::endl;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++)
+                std::cin >> maze[i][j];
+        }
+    }
+
+    std::cout << "The maze is: " << std::endl;
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++)
+            std::cout << maze[i][j] << " ";
+        std::cout << std::endl;
+    }
+
+    int entranceX, entranceY;
+    std::cout << "Set the entrance coordinates (x y)" << std::endl;
+
+    do {
+        std::cout << "x: ";
+        std::cin >> entranceX;
+        std::cout << "y: ";
+        std::cin >> entranceY;
+    } while (entranceX >= 0 && entranceX < m && entranceY >= 0 && entranceY < n);
+
+    int exitX, exitY;
+    std::cout << "Set the entrance coordinates (x y)" << std::endl;
+
+    do {
+        std::cout << "x: ";
+        std::cin >> exitX;
+        std::cout << "y: ";
+        std::cin >> exitY;
+    } while (exitX >= 0 && exitX < m && exitY >= 0 && exitY < n);
+
+
+
+
+
 }
