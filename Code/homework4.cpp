5162c73dbc8882c5e1f9363697c0e8947c926fde
deBroglieWave <zhihangyi0224@gmail.com>
2025-11-15 17:20:50 +0800

    finished tri-array representation

--- Code/homework4.cpp
+++ Code/homework4.cpp
@@ -5,12 +5,16 @@
 #include "LinkedList.h"
 
 void crossRepresentation();
+void triRepresentation();
+
 
 int main(void) {
     crossRepresentation();
+    triRepresentation();
 
     return 0;
 }
+
 
 void crossRepresentation() {
     LinkedList<char>* crossLinkedList = new LinkedList<char>();
@@ -61,17 +65,47 @@ void crossRepresentation() {
 
     int size = crossLinkedList->size();
 
+    std::cout << "Cross Linked List Representation:" << std::endl;
+
     for (int idx = 0; idx < size; idx++) {
         auto node = (CrossNode<char>*)(*crossLinkedList)[idx];
-        std::cout << "(" << node->getColumn() << ", " << node->getRow() << "): " << node->getValue();
+        std::cout << "(" << node->getColumn() << ", " << node->getRow() << "): " << node->getValue() << std::endl;
 
         if (node->getRight())
-            std::cout << "\t" << "(" << node->getRight()->getRow() << ", " << node->getRight()->getColumn() << "): " << node->getRight()->getValue() << std::endl;
+            std::cout << "right: (" << node->getRight()->getRow() << ", " << node->getRight()->getColumn() << "): " << node->getRight()->getValue() << std::endl;
         if (node->getDown())
-            std::cout << "(" << node->getDown()->getRow() << ", " << node->getDown()->getColumn() << "): " << node->getDown()->getValue() << std::endl;
+            std::cout << "down: (" << node->getDown()->getRow() << ", " << node->getDown()->getColumn() << "): " << node->getDown()->getValue() << std::endl;
 
         std::cout << std::endl;
     }
 
     delete crossLinkedList;
+}
+
+
+void triRepresentation() {
+    LinkedList<char>* triLinkedList = new LinkedList<char>();
+
+    triLinkedList->append(new TriNode<char>(0, 0, 'a'));
+    triLinkedList->append(new TriNode<char>(0, 4, 'b'));
+    triLinkedList->append(new TriNode<char>(1, 0, 'c'));
+    triLinkedList->append(new TriNode<char>(1, 1, 'd'));
+    triLinkedList->append(new TriNode<char>(1, 5, 'e'));
+    triLinkedList->append(new TriNode<char>(2, 2, 'f'));
+    triLinkedList->append(new TriNode<char>(2, 4, 'g'));
+    triLinkedList->append(new TriNode<char>(3, 1, 'h'));
+    triLinkedList->append(new TriNode<char>(3, 4, 'i'));
+    triLinkedList->append(new TriNode<char>(3, 5, 'j'));
+    triLinkedList->append(new TriNode<char>(5, 0, 'k'));
+    triLinkedList->append(new TriNode<char>(5, 2, 'l'));
+    triLinkedList->append(new TriNode<char>(5, 4, 'm'));
+
+    std::cout << "Tri-Array Representation:" << std::endl;
+
+    for (int idx = 0; idx < triLinkedList->size(); idx++) {
+        auto node = (TriNode<char>*)(*triLinkedList)[idx];
+        std::cout << "(" << node->getColumn() << ", " << node->getRow() << "): " << node->getValue() << std::endl;
+    }
+
+    delete triLinkedList;
 }
